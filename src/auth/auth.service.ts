0f8b713b35d7108a926359587d57f6b5b26066dc
Tirth <tirthacodes@gmail.com>
2023-08-21 21:58:56 +0545

    modified auth service and auth entity

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,8 @@ import { UserRepository } from './user.repo';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthDto } from './dtos/auth-dto';
 import { User } from './entity/user.entity';
+import { Repository } from 'typeorm';
+import { existsSync } from 'fs';
 
 @Injectable()
 export class AuthService {
@@ -16,10 +18,24 @@ export class AuthService {
 //     return this.userRepository.signUp(authDto);
 // }
 
+    constructor(
+        @InjectRepository(User) private repo: Repository<User>
+    ){}
 
 
     async signUp(authDto: AuthDto): Promise<void> {
         const {username, password } = authDto;
+
+        const exists = this.repo.findOne({
+            where: {
+                username: username
+            }
+        });
+
+        if(exists){
+            //throw some error
+        }
+
         const user = new User();
         user.username = username;
         user.password = password;
--- src/auth/entity/user.entity.ts
+++ src/auth/entity/user.entity.ts
@@ -1,6 +1,7 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
 
 @Entity({ name: 'users'})
+@Unique(['username'])
 export class User extends BaseEntity{
 
     @PrimaryGeneratedColumn()
